2f7b04beee06f8458a0703d75796534f6bbb2f42
arc-research-analytics <wwright@atlantaregional.org>
2024-05-06 06:50:35 -0400

    initial commit

---
+++ gwinnett_dash.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+import pydeck as pdk
+
+# global variable for county name
+county_var = 'Gwinnett'
